5fd74c327c5e882d2c16025ca61d19894cfd9134
Daniel Lirio Reis <daniellirioreis@gmail.com>
2017-04-03 18:17:21 -0300

    Mailgun

--- app/mailers/evaluation_mailer.rb
+++ app/mailers/evaluation_mailer.rb
@@ -1,6 +1,8 @@
 class EvaluationMailer < ApplicationMailer
   def notice(evaluation)
      @evaluation = evaluation
-     mail :to => "#{evaluation.student.email}", :from => "#{evaluation.company.email}", :subject => "Avaliação física há vencer"
+     # mail :to => "#{evaluation.student.email}", :from => "#{evaluation.company.email}", :subject => "Avaliação física há vencer"
+     mail to: "daniellirioreis@gmail.com"     
    end  
+   
 end
