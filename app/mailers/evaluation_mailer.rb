6ca9f97353f238e31cc2411fde9ea226d0ddf23a
Daniel Lirio Reis <daniellirioreis@gmail.com>
2017-04-03 19:41:07 -0300

    Voltando para configuração normal

--- app/mailers/evaluation_mailer.rb
+++ app/mailers/evaluation_mailer.rb
@@ -1,8 +1,7 @@
 class EvaluationMailer < ApplicationMailer
   def notice(evaluation)
      @evaluation = evaluation
-     # mail :to => "#{evaluation.student.email}", :from => "#{evaluation.company.email}", :subject => "Avaliação física há vencer"
-     mail to: "daniellirioreis@gmail.com"     
+     mail :to => "#{evaluation.student.email}", :from => "#{evaluation.company.email}", :subject => "Avaliação física há vencer"
    end  
    
 end
