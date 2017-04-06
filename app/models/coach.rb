08d329d1839789099ca5302c8e3314b51189fddf
Daniel Lirio Reis <daniellirioreis@gmail.com>
2017-04-05 22:59:39 -0300

    Criação do login do personal

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,9 +1,34 @@
 class Coach < ActiveRecord::Base
-  
+  has_one :user	
 	validates :name, :email, presence: true
 	validates :name, :email, :cpf, uniqueness: true
+	
+	after_save :create_user
 	
 	def to_s
 	 name
 	end	
+	
+	private
+	
+	def create_user
+		 user = User.find_by_email_and_coach_id(email, id)		 
+     unless user.present?
+       user = User.find_by_email(email)  		 
+
+       if user.present? 
+         errors.add(:email, "já em está em uso")          
+        else
+          if self.user.nil?
+            user = User.new(email: email, password: 12345678, password_confirmation: 12345678, coach_id: id)
+            if user.save
+            else
+            end            
+          else
+            self.user.update_attributes(email: email)                  
+          end    
+        end 
+     end 
+	end
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   belongs_to :student
   belongs_to :evaluator
+  belongs_to :coach
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :timeoutable
@@ -15,6 +16,10 @@ class User < ActiveRecord::Base
   
   def evaluator?
     evaluator.present?
+  end
+
+  def coach?
+    coach.present?
   end
   
 end
