8a8981b733597d5a4cb694b893aab88d25e8dda8
Daniel Lirio Reis <daniellirioreis@gmail.com>
2017-04-06 22:41:26 -0300

    Inicio da configuração do envio de email do resultado da avaliação

--- app/mailers/evaluation_mailer.rb
+++ app/mailers/evaluation_mailer.rb
@@ -1,7 +1,12 @@
 class EvaluationMailer < ApplicationMailer
   def notice(evaluation)
      @evaluation = evaluation
-     mail :to => "#{evaluation.student.email}", :from => "#{evaluation.company.email}", :subject => "Avaliação física há vencer"
+     mail :to => "#{evaluation.student.email}", :from => "#{evaluation.company.email}", :subject => "Avaliação física à vencer"
    end  
+
+   def result(evaluation)
+      @evaluation = evaluation
+      mail :to => "#{evaluation.student.email}", :from => "#{evaluation.company.email}", :subject => "Resultado da Avaliação"
+    end  
    
 end
