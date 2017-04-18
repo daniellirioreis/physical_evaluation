bf7da8c36cb2444a25b6d3d0ce0aba2b02682cc4
Daniel Lirio Reis <daniellirioreis@gmail.com>
2017-04-18 11:13:23 -0300

    Mostra de uma forma mais dinâmica os exercícios

--- app/models/exercise_training.rb
+++ app/models/exercise_training.rb
@@ -2,6 +2,8 @@ class ExerciseTraining < ActiveRecord::Base
   belongs_to :exercise
   belongs_to :training
   delegate :student, to: :training
+  delegate :grupo_muscular, :musculo, to: :exercise
+  
   delegate :student_id, to: :student
   
   validates :exercise_id, presence: :true
